d02d91011c04e38df2bd148f896b1a16f4d3a35c
LudovicoPellecchia <ludovico.pellecchia@gmail.com>
2023-08-24 15:44:08 +0200

    push iniziale

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script >
+import TemplateComponent from "./components/TemplateComponent.vue"
+
+export default{
+    components:{
+        TemplateComponent,
+    },
+    data(){
+        return{
+        }
+    }
+}
+</script>
+
+<template>
+
+</template>
+
+<style >
+</style>
